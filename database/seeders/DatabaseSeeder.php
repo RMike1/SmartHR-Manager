83d375ec709a42797828afa9a66f9bc7f20c30ab
RMike1 <71224316+RMike1@users.noreply.github.com>
2024-12-23 02:39:37 +0200

    adding category..

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,8 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        Category::factory(10)->create();
         User::factory()->create([
             'first_name' => 'Jon',
             'last_name' => 'Snow',
